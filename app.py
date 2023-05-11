1ae519b1fbeb7ad13d810d6f2ed2e245a92183cb
Saurabh <66420052+saurabhg2083@users.noreply.github.com>
2023-05-11 16:37:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask,request,render_template,jsonify
 from src.pipeline.prediction_pipeline import CustomData,PredictPipeline
 
 application=Flask(__name__)
-
 app=application
 
 @app.route('/')
